e8d15200066bd1f681c15f799baea9da39a10abf
ScorpionOrange <scorpionorange@126.com>
2017-01-03 00:02:26 +0800

    update in 2017.1.03

--- src/main/java/MaxSubSum.java
+++ src/main/java/MaxSubSum.java
@@ -13,7 +13,37 @@ public class MaxSubSum {
         int maxSum = 0;
 
         for(int i = 0; i < a.length; i++){
-            for(int j = i; )
+            for(int j = i; j < a.length; j++){
+                int thisSum = 0;
+
+                for(int k = i; k <= j; k++){
+                    thisSum = thisSum + a[k];
+                }
+
+                if(thisSum > maxSum){
+                    maxSum = thisSum;
+                }
+            }
+        }
+        return maxSum;
+    }
+
+    /**
+     * Quadratic maximum contiguous subsequence sum algorithm.
+     */
+    public static int maxSubSum2(int[] a){
+        int maxSum = 0;
+
+        for(int i = 0; i < a.length; i++){
+            int thisSum = 0;
+
+            for(int j = i; i < a.length; j++){
+                thisSum = thisSum + a[j];
+
+                if(thisSum > maxSum){
+                    maxSum = thisSum;
+                }
+            }
         }
         return maxSum;
     }
