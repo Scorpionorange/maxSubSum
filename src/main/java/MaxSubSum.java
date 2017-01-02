f7be23fc5cd1fc257457b31c310f2e79d961f4d3
ScorpionOrange <scorpionorange@126.com>
2017-01-02 23:54:39 +0800

    Initial commit

---
+++ src/main/java/MaxSubSum.java
@@ -0,0 +1,20 @@
+/**
+ * Created by ScorpionOrange on 2017/01/02.
+ */
+public class MaxSubSum {
+    public static void main(String[] args){
+        System.out.println("Testing");
+    }
+
+    /**
+     * Cubic maximum contiguous subsequence sum algorithm.
+     */
+    public static int maxSubSum1(int[] a){
+        int maxSum = 0;
+
+        for(int i = 0; i < a.length; i++){
+            for(int j = i; )
+        }
+        return maxSum;
+    }
+}
