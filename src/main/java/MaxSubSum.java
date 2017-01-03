8bd8da57aed12a0af6a6fcc68f4e5614dcfa068c
ScorpionOrange <scorpionorange@126.com>
2017-01-03 22:43:55 +0800

    update in 2017.1.03 with maxSubSum3.

--- src/main/java/MaxSubSum.java
+++ src/main/java/MaxSubSum.java
@@ -47,4 +47,50 @@ public class MaxSubSum {
         }
         return maxSum;
     }
+
+    /**
+     * Recursive maximum contiguous subsequence sum algorithm.
+     * Finds maximum sum in subarray spanning a[left...right].
+     * Does not attempt to maintain actual best sequence.
+     */
+    private static int maxSumRec(int[] a, int left, int right){
+        if(left == right) {// Base case
+            if (a[left] > 0) {
+                return a[left];
+            } else {
+                return 0;
+            }
+        }
+        int center = (left + right) / 2;
+        int maxLeftSum = maxSumRec(a, left, center);
+        int maxRightSum = maxSumRec(a, center + 1, right);
+
+        int maxLeftBorderSum = 0, leftBorberSum = 0;
+        for( int i = center; i >= left; i--){
+            leftBorberSum += a[i];
+            if(leftBorberSum > maxLeftBorderSum){
+                maxLeftBorderSum = leftBorberSum;
+            }
+        }
+
+        int maxRightBorderSum = 0, rightBorderSum = 0;
+        for(int i = center + 1; i <= right; i++){
+            rightBorderSum += a[i];
+            if(rightBorderSum > maxRightBorderSum){
+                maxRightBorderSum = rightBorderSum;
+            }
+        }
+
+        int theMax = maxLeftSum > maxRightSum ? maxLeftSum : maxRightSum;
+        theMax = theMax > (maxLeftBorderSum + maxRightBorderSum) ? theMax : (maxLeftBorderSum + maxRightBorderSum);
+        return theMax;
+    }
+
+    /**
+     * Driver for divide-and-conquer maximum contiguous
+     * subsequence sum algorithm.
+     */
+    public static int maxSubSum3(int[] a){
+        return maxSumRec(a, 0, a.length - 1);
+    }
 }
